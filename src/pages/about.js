879f5cb94fb58ea41ae1ee84ef80fc870d77e212
nicolatournay <nicolatournay@outlook.com>
2022-01-31 19:54:50 +0100

    Pessimistic trial.

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>There is no 'me', properly speaking. Only an ever-changing memeplex painfully building the simulacra of a cohesive ego.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
---
+++ src/pages/contact-me.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const ContactMe =    () => {
+  return (
+    <Layout pageTitle="Contact Me">
+      <p>You cannot contact me. Communication is impossible. The void between us is too big. We are born alone and we die alone.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default ContactMe
